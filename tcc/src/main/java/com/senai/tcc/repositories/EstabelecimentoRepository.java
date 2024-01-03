83cbe49897e190494bafc7b56c950ac3091432f8
Breno <Breno@DESKTOP-04FHHE0>
2024-01-03 11:16:53 -0300

    Mudanças do banco para o postgreSql

--- tcc/src/main/java/com/senai/tcc/repositories/EstabelecimentoRepository.java
+++ tcc/src/main/java/com/senai/tcc/repositories/EstabelecimentoRepository.java
@@ -13,25 +13,27 @@ import com.senai.tcc.entities.Estabelecimento;
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
 
-	@Query(value = "select top 3 * from estabelecimento" + " where ativo = 'S'", nativeQuery = true)
+	@Query(value = "select * from estabelecimento " + " where ativo = 'S' limit 3", nativeQuery = true)
 	List<Estabelecimento> obterAcessados();
 
-	@Query(value = "Select TOP 3 * \r\n" + "FROM estabelecimento \r\n"
-			+ "where ativo = 'S' and (tipo_id = :tipoId or :tipoId is null) ", nativeQuery = true)
+	@Query(value = "Select  * \r\n" + "FROM estabelecimento \r\n"
+			+ "where ativo = 'S' and (tipo_id = COALESCE(:tipoId, tipo_id) OR :tipoId IS NULL) limit 3 ", nativeQuery = true)
 	List<Estabelecimento> obterAcessadosFiltrados(@Param("tipoId") Long tipoId);
+	
+	
 
 	@Query(value = "select * from estabelecimento\r\n" + "where cnpj = :cnpj and ativo = 'S'", nativeQuery = true)
 	Optional<Estabelecimento> findByCnpj(@Param("cnpj") String cnpj);
 
-	@Query(value = "select * from estabelecimento where nome like %:nome% and ativo = 'S'", nativeQuery = true)
+	@Query(value = "select * from estabelecimento where nome LIKE  %:nome% and ativo = 'S'", nativeQuery = true)
 	List<Estabelecimento> obterEstabsPeloNome(@Param("nome") String nome);
 
-	@Query(value = "select top(3) * from estabelecimento, endereco " + "where ativo = 'S' and "
+	@Query(value = "select  * from estabelecimento, endereco " + "where ativo = 'S' and "
 			+ "estabelecimento.enderecoid = endereco.endereco_id and" + "(nome like %:nome% or :nome is null) and "
 			+ "(banheiro_acessivel = :banheiro or :banheiro is null) and "
 			+ "(rampa_acessivel = :rampa or :rampa is null) and "
 			+ "(estacionamento_acessivel = :estac or :estac is null) and " + "(uf like %:uf% or :uf is null) and "
-			+ " (localidade like %:localidade% or :localidade is null)", nativeQuery = true)
+			+ " (localidade like %:localidade% or :localidade is null) limit 3", nativeQuery = true)
 	List<Estabelecimento> obterEstabsFiltrados(@Param("nome") String nome, @Param("banheiro") String banheiro,
 			@Param("rampa") String rampa, @Param("estac") String estac, @Param("uf") String uf,
 			@Param("localidade") String localidade);
